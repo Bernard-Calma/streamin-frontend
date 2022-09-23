f1b43b64927ce46a15e3ad385a38edd7dbff6b00
rbidon <roussebidon@gmail.com>
2022-09-22 21:11:21 -0400

    Added search component, signin component and addded styling in App.css

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// Import FontAwesome icon for the search bar
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
+const SeachBar =() => {
+    return(
+        <form action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden"></span>
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            placeholder="search your video"
+            name="s" 
+        />
+        <button type="submit">
+            Submit
+            {/* <FontAwesomeIcon icon={solid( "magnifying-glass")} /> */}
+        </button>
+    </form>
+    )
+}
+export default SeachBar
