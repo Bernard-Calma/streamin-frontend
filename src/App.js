aa07da03bb7c5ce8e430b38f8fb1a54fd2eab226
rbidon <roussebidon@gmail.com>
2022-09-22 22:24:44 -0400

    fixing merge conflict

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 // Import bootstrap css
 // import 'bootstrap/dist/css/bootstrap.min.css';
-// Import the Header Components 
+
+// Import the Header/Navbar Component
 import Header from './components/Header'
-//Import the Footer Components
+//Import the Footer Component
 import Footer from './components/Footer'
+// Import the signInHeader Component
+import SignInHeader from './components/signInHeader'
+//Import the aboutPageHeader Component
+import AboutPageHeader from './components/AboutPageHeader'
+
 function App() {
   return (
     <div className="App">
       {/* Added the footer and header component that will display on the homepage & User page */}
       {/* <Header/>
       <Footer/> */}
+      {/* SignIn Header Component that has Searchbar Component*/}
+      {/* <SignInHeader/> */}
+      {/* Aboutpage Header Component */}
+      <aboutPageHeader/>
     </div>
   );
 }
