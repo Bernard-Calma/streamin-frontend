2b15efc1d8da326eeb1c23ce7b0e356103621a90
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-09-28 21:42:57 -0500

    About working and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,64 @@
 import React, {Component} from 'react'
 // Import the Font Awesome bar that will change
 
+//Import About
+import About from './About';
+
 class Header extends Component  {
-    // constructor(props) {
-    //     super(props)
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            showAbout: true
+        }
+    }
+
+    showAbout = () => {
+        if(this.state.showAbout === true) {
+            this.setState({
+                showAbout: false
+            })
+        } else {
+            this.setState({
+                showAbout: true
+            })
+        }
+    }
+
     render(){
-        return(  
-       <div className="header container-fluid ">
-            <nav className="navbar">
-                {/* Menu Icon that will show at a specific media query */}
-                <div className="menu-icon">
-                    {/* Will add an icon */}
+        return(
+            <div>
+                <div className="header container-fluid ">
+                        <nav className="navbar">
+                            {/* Menu Icon that will show at a specific media query */}
+                            <div className="menu-icon">
+                                {/* Will add an icon */}
+                            </div>
+                            
+                            <ul className="nav-menu">
+                            <div id="navLogo">
+                            <a className="homepageLink" href="#">
+                                {/* Add image or App title  */}
+                                Streamin
+                            </a>
+                            </div>
+                                <li className="nav-item">
+                                    <a
+                                        id="nav-link"
+                                        className="nav-link"
+                                        onClick={this.showAbout}
+                                    > 
+                                    About
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
                 </div>
-                
-                <ul className="nav-menu">
-                <div id="navLogo">
-                <a className="homepageLink" href="#">
-                    {/* Add image or App title  */}
-                    Streamin
-                </a>
-                </div>
-                    <li className="nav-item">
-                        <a id="nav-link" className="nav-link"> 
-                        About
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-       </div>
-    );
+                {this.state.showAbout === true
+                    ? <About />
+                    : <></>
+                }
+            </div>
+        );
     }
 }
 
