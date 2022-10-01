810420e15e6cbea1a4b17246ec0d3685969a73d4
nard.calma@gmail.com <nard.calma@gmail.com>
2022-10-01 11:41:22 -0700

    changed SearchBar component from fucntion to class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,26 +3,34 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
-const SeachBar =() => {
-    return(
-        <div className="searchBar">
-        <form >
-        <label htmlFor="header-search">
-            <span className="visually-hidden"></span>
-        </label>
-        <input
-            type="text"
-            id="header-search"
-            placeholder="search your video"
-            name="s"
-            
-        />
-         <button type="submit"className="fontIcon">
-            {/* Search */}
-             <FontAwesomeIcon   icon={solid( "magnifying-glass")} />
-        </button>
-    </form>
-    </div>
-    )
+class SeachBar extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div className="searchBar">
+            <form >
+            <label htmlFor="header-search">
+                <span className="visually-hidden"></span>
+            </label>
+            <input
+                type="text"
+                id="header-search"
+                placeholder="search your video"
+                name="s"
+                
+            />
+             <button type="submit"className="fontIcon">
+                {/* Search */}
+                 <FontAwesomeIcon   icon={solid( "magnifying-glass")} />
+            </button>
+        </form>
+        </div>
+        )
+    }  
 }
 export default SeachBar
