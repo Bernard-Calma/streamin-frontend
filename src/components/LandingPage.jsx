15b5eeb32eea359f1a2ce91aba77d5ab386a31bc
Bernard Calma <nard.calma@gmail.com>
2023-05-07 01:35:25 -0700

    feat(landingPage): Add api call to grab video array from server
    Added videolist state and created get video list function in useEffect to grab all videos on load

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const LandingPage = () => {
+    const [videoList, setVideoList] = useState([])
+    
+    useEffect( () => {
+        const handleGetVideoList = () => {
+            axios({
+                method: "GET",
+                url: `${process.env.REACT_APP_SERVER_URL}/videos`
+            })
+            .then(res => {
+                // console.log(res.data)
+                setVideoList(res.data)
+            })
+            .catch(err => console.log(err))
+        }
+
+        handleGetVideoList()
+    },[])
     return (
         <main>
-            
+
         </main>
     )
 }
