a5792bca4a2c92a8c066ea21a5d2c467cdfd147c
Bernard Calma <nard.calma@gmail.com>
2023-05-10 23:53:43 -0700

    refractor(App): Chane App to class to functional component
    Change app to functional component and switch this.state to setState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {Component} from "react"
+import React , {Component, useState} from "react"
 import './App.css';
 
 // Components
@@ -15,289 +15,282 @@ import User from "./components/User/user";
 
 // Import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Show from "./views/Show/Show";
 import axios from "axios";
 
 //URL
 let baseURL = process.env.REACT_APP_SERVER_URL
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      user: {
-        id: "Guest",
-        username: "Guest",
-        name: "Guest"
-      },
-      show: "Landing Page",
-      loginMessage: "",
-      videoId: "",
-      search: "",
-      // view pages
-      register: false,
-      loggedIn: false,
-      showMain: false,
-      showVideoList: true,
-      createVideo: false,
-      modifyVideo: false,
-      searchVideos: false,
-      // modify video
-      videoToModify: "",
-      searchedVideos: [],
-      // Views
-      showLanding: true,
-      showLogin: false,
-      showAbout: false,
-      // Videos
-      videoList: [],
-    }
-  }
+const App = () => {
+  const [user, setUser] = useState({
+    user: {
+      id: "Guest",
+      username: "Guest",
+      name: "Guest"
+    },
+  })
 
-  handleChange = (e) => {
-    // console.log("test ", e.target)
-    e.preventDefault();
-    this.setState({
-        [e.target.name]: e.target.value,
-        [e.target.name]: e.target.value,
-    })
-  }
+  const [showLanding, setShowLanding] = useState(true)
+  const [showLogin, setShowLogin] = useState(false)
+  const [showAbout, setShowAbout] = useState(false)
 
-  handleLogin = (e) => {
-    e.preventDefault();
-    // console.log(baseURL)
-    fetch(`${baseURL}/users/login/${this.state.username}/${this.state.password}`)
-    .then(res => {
-        if(res.status === 200) {
-            return res.json();
-        } else { 
-          return []
-        }
-    })
-    .then(data => {
-      if(data.length === 0) {
-        this.setState({
-          loggedIn: false,
-          loginMessage: "User not found."
-        }) 
-      } else {
-        this.setState({
-          user: data,
-          loggedIn: true,
-      })  
-      }
+  const [videoList, setVideoList] =useState([])
+
+
+  //   state = {      
+  //     show: "Landing Page",
+  //     loginMessage: "",
+  //     videoId: "",
+  //     search: "",
+  //     // view pages
+  //     register: false,
+  //     loggedIn: false,
+  //     showMain: false,
+  //     showVideoList: true,
+  //     createVideo: false,
+  //     modifyVideo: false,
+  //     searchVideos: false,
+  //     // modify video
+  //     videoToModify: "",
+  //     searchedVideos: [],
+  //   }
+  // }
+
+  // handleChange = (e) => {
+  //   // console.log("test ", e.target)
+  //   e.preventDefault();
+  //   setState({
+  //       [e.target.name]: e.target.value,
+  //       [e.target.name]: e.target.value,
+  //   })
+  // }
+
+  // handleLogin = (e) => {
+  //   e.preventDefault();
+  //   // console.log(baseURL)
+  //   fetch(`${baseURL}/users/login/${username}/${password}`)
+  //   .then(res => {
+  //       if(res.status === 200) {
+  //           return res.json();
+  //       } else { 
+  //         return []
+  //       }
+  //   })
+  //   .then(data => {
+  //     if(data.length === 0) {
+  //       setState({
+  //         loggedIn: false,
+  //         loginMessage: "User not found."
+  //       }) 
+  //     } else {
+  //       setState({
+  //         user: data,
+  //         loggedIn: true,
+  //     })  
+  //     }
         
-    })
-  }
+  //   })
+  // }
 
-  handleSignOut = (e) => {
-    e.preventDefault()
-    this.setState({
-        loggedIn : false
-    })
-  }
+  // handleSignOut = (e) => {
+  //   e.preventDefault()
+  //   setState({
+  //       loggedIn : false
+  //   })
+  // }
 
-  showAbout = () => {
-    if(this.state.showAbout === true) {
-      this.setState({
-        showAbout: false
-      })
-    } else {
-      this.setState({
-        showAbout: true
-      })
-    }
-  }
+  // showAbout = () => {
+  //   if(showAbout === true) {
+  //     setState({
+  //       showAbout: false
+  //     })
+  //   } else {
+  //     setState({
+  //       showAbout: true
+  //     })
+  //   }
+  // }
 
-  handleRegister = (e) => {
-    e.preventDefault();
-    if(this.state.password !== this.state.passwordCheck){
-      for(const child of e.target.querySelectorAll("input"))
-        if(child.type === "password")
-        child.value = ""; 
-    this.setState({
-      loginMessage: "Password does not match."
-      })
-      return;
-    }
-    // console.log(baseURL)
-    fetch(`${baseURL}/users/`, {
-        method: "POST",
-        body: JSON.stringify(this.state),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(res => {
-      return res.json()})
-    .then(resJson => {
-        if(!resJson._id) {
-          this.setState({
-            loginMessage: "Username is already taken.",
-          })
-          // console.log(e.target.querySelectorAll("input"))
-          for(const child of e.target.querySelectorAll("input"))
-            if(child.type === "password")
-            child.value = ""; 
-        } else {
-          // console.log("JSon", resJson)
-          this.setState({
-            username: "",
-            password: "",
-            name: "",
-            user: resJson,
-            loggedIn: true,
-            register: false,
-            loginMessage: "Account Created."
-          })
-        } 
+  // handleRegister = (e) => {
+  //   e.preventDefault();
+  //   if(password !== passwordCheck){
+  //     for(const child of e.target.querySelectorAll("input"))
+  //       if(child.type === "password")
+  //       child.value = ""; 
+  //   setState({
+  //     loginMessage: "Password does not match."
+  //     })
+  //     return;
+  //   }
+  //   // console.log(baseURL)
+  //   fetch(`${baseURL}/users/`, {
+  //       method: "POST",
+  //       body: JSON.stringify(state),
+  //       headers: {
+  //           'Content-Type': 'application/json'
+  //       }
+  //   }).then(res => {
+  //     return res.json()})
+  //   .then(resJson => {
+  //       if(!resJson._id) {
+  //         setState({
+  //           loginMessage: "Username is already taken.",
+  //         })
+  //         // console.log(e.target.querySelectorAll("input"))
+  //         for(const child of e.target.querySelectorAll("input"))
+  //           if(child.type === "password")
+  //           child.value = ""; 
+  //       } else {
+  //         // console.log("JSon", resJson)
+  //         setState({
+  //           username: "",
+  //           password: "",
+  //           name: "",
+  //           user: resJson,
+  //           loggedIn: true,
+  //           register: false,
+  //           loginMessage: "Account Created."
+  //         })
+  //       } 
         
-    })
-  }
+  //   })
+  // }
 
 
-  onClickVideo = (e) => {
-    e.preventDefault();
-    // console.log(e.target.id)
-    this.setState({
-        showVideoList: false,
-        videoId: e.target.id,
-    })
-  }
+  // onClickVideo = (e) => {
+  //   e.preventDefault();
+  //   // console.log(e.target.id)
+  //   setState({
+  //       showVideoList: false,
+  //       videoId: e.target.id,
+  //   })
+  // }
 
-  handleCreateSubmit = (e) =>{
-    e.preventDefault();
-    // console.log("create video")
-    this.setState({
-        createVideo: true,
-        showVideoList: false,
-    })
-  }
+  // handleCreateSubmit = (e) =>{
+  //   e.preventDefault();
+  //   // console.log("create video")
+  //   setState({
+  //       createVideo: true,
+  //       showVideoList: false,
+  //   })
+  // }
 
-  // Being used on Crate and Modify Forms
-  handleCreateReturn = () => {
-    this.setState({
-        createVideo: false,
-        showVideoList: true,
-        modifyVideo: false,
-        videoToModify: "",
-    })
-  }
+  // // Being used on Crate and Modify Forms
+  // handleCreateReturn = () => {
+  //   setState({
+  //       createVideo: false,
+  //       showVideoList: true,
+  //       modifyVideo: false,
+  //       videoToModify: "",
+  //   })
+  // }
 
-  handleModifyVideo = (e) => {
-    e.preventDefault()
-    // console.log("Modify Video", e.target.parentNode.parentNode.firstChild.id)
-    let videoID = e.target.parentNode.parentNode.firstChild.id;
-    this.setState({
-            showMain: false,
-            modifyVideo: true,
-            videoToModify: videoID,
-        }
+  // handleModifyVideo = (e) => {
+  //   e.preventDefault()
+  //   // console.log("Modify Video", e.target.parentNode.parentNode.firstChild.id)
+  //   let videoID = e.target.parentNode.parentNode.firstChild.id;
+  //   setState({
+  //           showMain: false,
+  //           modifyVideo: true,
+  //           videoToModify: videoID,
+  //       }
         
-    )
+  //   )
+  // }
+
+  // onClickRegister = (e) => {
+  //   e.preventDefault();
+  //   setState({
+  //       register: true,
+  //   })
+  // // console.log(baseURL)
+  // }
+
+//   const handleSearch = (e) => {
+//     e.preventDefault()
+//     fetch(`${baseURL}/videos/search/${search}`)
+//     .then(res => {
+//         if(res.status === 200) return res.json()
+//         return ""
+//     })
+//     .then(data => {
+//         setState({
+//           searchedVideos: data,
+//           searchVideos: true,
+//           showVideoList: true,
+//         })
+//     })
+// }
+
+ const handleShowLandingPage = () => {
+    setShowLanding(!showLanding)
+    setShowAbout(false)
   }
 
-  onClickRegister = (e) => {
-    e.preventDefault();
-    this.setState({
-        register: true,
-    })
-  // console.log(baseURL)
-  }
+  const handleToggleLoginPage = () => setShowLogin(!showLogin)
+  
+  const handleToggleAbout = () => setShowAbout(!showAbout)
+  
+  const handleChangeUser = newUser => setUser(newUser)
 
-  handleSearch = (e) => {
-    e.preventDefault()
-    fetch(`${baseURL}/videos/search/${this.state.search}`)
-    .then(res => {
-        if(res.status === 200) return res.json()
-        return ""
-    })
-    .then(data => {
-        this.setState({
-          searchedVideos: data,
-          searchVideos: true,
-          showVideoList: true,
+  const componentDidMount = async () => {
+        await axios({
+            method: "GET",
+            url: `${process.env.REACT_APP_SERVER_URL}/videos`
         })
-    })
-}
+        .then(res => {
+            // console.log(res.data)
+            setVideoList(res.data)
+        })
+        .catch(err => console.log(err));
+    }
 
-  handleShowLandingPage = () => {
-    this.setState({showLanding: !this.state.showLanding})
-    this.setState({showAbout: false})
-  }
-
-  handleToggleLoginPage = () => this.setState({showLogin: !this.state.showLogin})
-  
-  handleToggleAbout = () => this.setState({showAbout: !this.state.showAbout})
-  
-  handleChangeUser = newUser => this.setState({user: newUser})
-
-  componentDidMount = () => {
-      const handleGetVideoList = async () => {
-          await axios({
-              method: "GET",
-              url: `${process.env.REACT_APP_SERVER_URL}/videos`
-          })
-          .then(res => {
-              // console.log(res.data)
-              this.setState({videoList: res.data})
-          })
-          .catch(err => console.log(err))
-      }
-
-      handleGetVideoList()
-  }
-
-  componendid
-
-  render() {
-    
     // if logged in is false
-    // return( !this.state.loggedIn
+    // return( !loggedIn
     //   ? <>
     //     
-    //     {this.state.showAbout === true
+    //     {showAbout === true
     //       ? <>
     //           <About/>
     //         </>      
     //       : <>
     //         <SignIn
-    //           handleChange = {this.handleChange}
-    //           handleLogin = {this.handleLogin}
-    //           handleRegister = {this.handleRegister}
-    //           message = {this.state.loginMessage}
-    //           onClickRegister = {this.onClickRegister}
-    //           register = {this.state.register}
+    //           handleChange = {handleChange}
+    //           handleLogin = {handleLogin}
+    //           handleRegister = {handleRegister}
+    //           message = {loginMessage}
+    //           onClickRegister = {onClickRegister}
+    //           register = {register}
     //         />
     //       </>
     //     }                 
     //     <Footer />
     //   </>
     //   : <>
-    //     {/* If login successful change state.loggedIn to true */}
+    //     {/* If login successful change loggedIn to true */}
     //     {/* // Pass in User Component  */}
     //     <SignInHeader 
-    //       signOut={this.handleSignOut} 
-    //       handleLogo = {this.handleLogo}
-    //       handleChange = {this.handleChange}
-    //       search = {this.state.search}
-    //       handleSearch = {this.handleSearch}
+    //       signOut={handleSignOut} 
+    //       handleLogo = {handleLogo}
+    //       handleChange = {handleChange}
+    //       search = {search}
+    //       handleSearch = {handleSearch}
     //       />
     //     <User 
-    //       key = {this.state.user._id} 
-    //       user = {this.state.user} 
-    //       showMain = {this.state.showMain} 
-    //       handleLogo = {this.handleLogo} 
-    //       onClickVideo = {this.onClickVideo}
-    //       showVideoList = {this.state.showVideoList}
-    //       videoId = {this.state.videoId}
-    //       handleCreateSubmit = {this.handleCreateSubmit}
-    //       createVideo = {this.state.createVideo}
-    //       videoToModify = {this.state.videoToModify}
-    //       handleCreateReturn = {this.handleCreateReturn}
-    //       handleModifyVideo = {this.handleModifyVideo}
-    //       modifyVideo = {this.state.modifyVideo}
-    //       searchVideos = {this.state.searchVideos}
-    //       searchedVideos = {this.state.searchedVideos}
+    //       key = {user._id} 
+    //       user = {user} 
+    //       showMain = {showMain} 
+    //       handleLogo = {handleLogo} 
+    //       onClickVideo = {onClickVideo}
+    //       showVideoList = {showVideoList}
+    //       videoId = {videoId}
+    //       handleCreateSubmit = {handleCreateSubmit}
+    //       createVideo = {createVideo}
+    //       videoToModify = {videoToModify}
+    //       handleCreateReturn = {handleCreateReturn}
+    //       handleModifyVideo = {handleModifyVideo}
+    //       modifyVideo = {modifyVideo}
+    //       searchVideos = {searchVideos}
+    //       searchedVideos = {searchedVideos}
     //     />     
     //     
     //   </>                               
@@ -305,26 +298,24 @@ class App extends Component {
     return(
       <>
         <Header
-          isShowingAbout = {this.state.showAbout}
-          user = {this.state.user}
-          showAbout = {this.showAbout}
-          handleLogo={this.handleLogo}
-          handleShowLandingPage={this.handleShowLandingPage}
-          handleToggleLoginPage={this.handleToggleLoginPage}
+          user = {user}
+          showAbout = {showAbout}
+          handleShowLandingPage={handleShowLandingPage}
+          handleToggleLoginPage={handleToggleLoginPage}
         />
         {
-            !this.state.showAbout
+            !showAbout
             ? <>
                 <LandingPage  
-                  handleChangeUser = {this.handleChangeUser}
-                  handleToggleLoginPage={this.handleToggleLoginPage}
-                  showLanding = {this.state.showLanding}
-                  showLogin = {this.state.showLogin}
-                  user = {this.state.user}
-                  videoList = {this.state.videoList}
+                  handleChangeUser = {handleChangeUser}
+                  handleToggleLoginPage={handleToggleLoginPage}
+                  showLanding = {showLanding}
+                  showLogin = {showLogin}
+                  user = {user}
+                  videoList = {videoList}
                 />
                 <Footer 
-                  handleToggleAbout = {this.handleToggleAbout}
+                  handleToggleAbout = {handleToggleAbout}
                 />
             </>
             : <>
@@ -334,7 +325,6 @@ class App extends Component {
         
       </>
     )
-  }
 }
 
 export default App;
