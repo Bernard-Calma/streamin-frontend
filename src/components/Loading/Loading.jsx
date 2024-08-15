7886538ca06eada4cb395ab9995992fd9a12a5bf
unknown <nard.calma@gmail.com>
2024-08-14 23:38:02 -0700

    style:(Change add video font and color)
    Change font family and background color on create video section and change loading icon to SVG

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import loading from '../../assets/loading.gif'
+import loading from '../../assets/loading.svg'
 import './Styles.css';
 
 const Loading = props => 
         <div className="loading">
-            { props.loading && <img src={loading} alt="" /> }  
+            { props.loading && <img src={loading} alt="loading" /> }  
         </div> 
 
 export default Loading
